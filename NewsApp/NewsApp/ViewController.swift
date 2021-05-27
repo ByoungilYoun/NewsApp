1ffddba8f7b4ee622180de8ecd67d206c4b63123
YounByoungil <jimmy7525@naver.com>
2021-05-28 01:09:30 +0900

    Setup Project

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 윤병일 on 2021/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+}
+
